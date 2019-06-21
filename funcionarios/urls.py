5937753cbf00c82aefcf8503801a7810368b476f
cagame <gabriel_vixbr@hotmail.com>
2019-06-21 17:40:42 -0300

    atualizando

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import FuncionarioCreate
+from .views import FuncionarioCreate, FuncionarioList, FuncionarioEdit, FuncionarioDelete
 
 
 urlpatterns = [
-    path('novo', FuncionarioCreate.as_view(), name='funcionario_create'),
+    path('novo', FuncionarioCreate, name='funcionario_create'),
+    path('list', FuncionarioList.as_view(), name='funcionario_list'),
+    path('edit/<int:pk>/', FuncionarioEdit.as_view(), name='funcionario_update'),
+    path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='funcionario_delete'),
 ]
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
 from .models import Funcionario
-from .forms import FuncionarioForm
-from empresas.models import Empresa
-from django.contrib.auth.models import User
+from .forms import FuncionarioForm, UsuarioForm
+from django.views.generic import ListView, UpdateView, DeleteView, View
 
 
-class FuncionarioCreate(CreateView):
+class FuncionarioList(ListView):
     model = Funcionario
-    form_class = FuncionarioForm
 
-    def form_valid(self, form):
-            funcionario = form.save(commit=False)
-            funcionario.empresa = Empresa.objects.create(nome=empresa)
-            funcionario.user = User.objects.create_user(username=usuario, password=senha)
-            funcionario.save()
-            return super(FuncionarioCreate, self).form_valid(form)
+    def get_queryset(self):
+        user = self.request.user.id
+        funcionario = Funcionario.objects.get(user=user)
+        empresa_logada = funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+def FuncionarioCreate(request):
+    funcionarioform = FuncionarioForm(request.POST or None, request.FILES or None)
+    usuarioform = UsuarioForm(request.POST or None, request.FILES or None)
+    user = request.user.id
+    funcionario = Funcionario.objects.get(user=user)
+    empresa = funcionario.empresa
+
+    if funcionarioform.is_valid():
+       funcionario = funcionarioform.save(commit=False)
+       user = usuarioform.save(commit=False)
+       user.set_password(user.password)
+       user.save()
+       funcionario.user = usuarioform.save()
+       funcionario.empresa = empresa
+       funcionario.save()
+       return redirect(FuncionarioCreate)
+
+    return render(request, 'funcionarios/funcionario_form.html', {'funcionarioform': FuncionarioForm, 'usuarioform': UsuarioForm})
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = 'funcionario_list'
